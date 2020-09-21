08394b1f2740a5d57089bf79e8da795f2de9572b
theoriginalsam <spoudel04@gmail.com>
2020-09-21 08:42:17 +0545

    Commit via Script

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 var express = require("express");
 var path = require("path");
+var mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://samir:samir@cluster0.sj8rp.mongodb.net/cart?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
 
 var app = express();
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to Mongoose");
+});
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+var PORT = 3000;
+app.listen(PORT, (req, res) => {
+  console.log(`Listening on PORT`);
+});
