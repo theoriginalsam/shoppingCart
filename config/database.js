0c056cade1f20c5a908b548c976179b14c9a3d0c
theoriginalsam <spoudel04@gmail.com>
2020-09-21 08:48:40 +0545

    Commit via Script

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  database:
+    "mongodb+srv://samir:samir@cluster0.sj8rp.mongodb.net/cart?retryWrites=true&w=majority",
+  PORT: 3000 || 5000,
+};
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var path = require("path");
 var mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://samir:samir@cluster0.sj8rp.mongodb.net/cart?retryWrites=true&w=majority",
-  { useUnifiedTopology: true, useNewUrlParser: true }
-);
+var config = require("./config/database");
+
+mongoose.connect(config.database, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 var app = express();
 
@@ -22,7 +24,6 @@ app.get("/", (req, res) => {
   res.send("Working");
 });
 
-var PORT = 3000;
-app.listen(PORT, (req, res) => {
-  console.log(`Listening on PORT`);
+app.listen(config.PORT, (req, res) => {
+  console.log(`Listening on PORT ${config.PORT}`);
 });
