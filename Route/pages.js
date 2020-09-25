a31b0827037ac1522c43acb348c082920316c841
theoriginalsam <spoudel04@gmail.com>
2020-09-25 23:42:35 +0545

    Commit via Script

--- Route/pages.js
+++ Route/pages.js
@@ -2,7 +2,7 @@ var express = require("express");
 var Router = express.Router();
 
 Router.get("/", (req, res) => {
-  res.render("index", {
+  res.render("/index", {
     title: "Home",
   });
 });
