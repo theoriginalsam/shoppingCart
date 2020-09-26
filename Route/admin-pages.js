9ad890b578f79dea8015d4faf49c29fbb34e9311
theoriginalsam <spoudel04@gmail.com>
2020-09-27 00:12:35 +0545

    Commit via Script

--- Route/admin-pages.js
+++ Route/admin-pages.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var Router = express.Router();
-
+Router.get("/pages", (req, res) => {
+  res.send("HEY you are in right place");
+});
 Router.get("/add_pages", (req, res) => {
   var title = "";
+  var slug = "";
+  var content = "";
   res.render("admin/add_pages", {
-    title: "Add a page",
+    title: title,
+    slug: slug,
+    content: content,
   });
 });
 
