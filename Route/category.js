eb4db09720527f73a8a04605993107a27129cb27
theoriginalsam <spoudel04@gmail.com>
2020-10-11 18:19:47 +0545

    Adding get for add category

--- Route/category.js
+++ Route/category.js
@@ -1,4 +1,5 @@
 var express = require("express");
+
 var Categories = require("../Model/category");
 var ObjectId = require("mongodb").ObjectID;
 var Router = express.Router();
@@ -15,10 +16,51 @@ Router.get("/add_category", (req, res) => {
   var title = "";
   var slug = "";
   
-  res.render("admin/", {
+  res.render("admin/add_category", {
     title: title,
     slug: slug,
    
   });
+});
+
+Router.post("/add_pages", (req, res) => {
+  req.checkBody("title", "must have a value").notEmpty();
+  req.checkBody("slug", "must have a value").notEmpty();
+  var title = req.body.title;
+  var slug = req.body.slug;
+  var errors = req.validationErrors();
+  if (errors) {
+    res.render("admin/add_category", {
+      errors: errors,
+      title: title,
+      slug: slug,
+     
+    });
+  } else {
+    Categories.findOne({ slug: slug }, (err, category) => {
+      if (category) {
+        res.render("admin/add_category", {
+          errors: errors,
+          title: title,
+          slug: slug,
+        
+        });
+      } else {
+        var page = new Pages({
+          title,
+          slug,
+          content,
+          sorting: 100,
+        });
+        page.save((err) => {
+          if (err) {
+            return console.log("Error");
+          }
+          console.log("Successfully added");
+          res.redirect("/admin");
+        });
+      }
+    });
+  }
 });
 module.exports = Router;
